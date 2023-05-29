4ca5909e95acde49a76d0edc7199289caf91e5dc
Bill <zhouzhenbiao@guanmai.cn>
2023-05-29 16:53:49 +0800

    chore: release v1.1.7-beta.3

--- scripts/generate-version.js
+++ scripts/generate-version.js
@@ -3,8 +3,9 @@ const path = require('path');
 
 const { version } = require('../package.json');
 
-fs.writeFileSync(
-  path.join(__dirname, '..', 'components', 'version', 'version.tsx'),
-  `export default '${version}'`,
-  'utf8',
-);
+/** 删除version 了，没什么用 */
+// fs.writeFileSync(
+//   path.join(__dirname, '..', 'components', 'version', 'version.tsx'),
+//   `export default '${version}'`,
+//   'utf8',
+// );
